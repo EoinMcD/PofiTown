423e1f51ac126a68cbcfc30bf6fcbd165ef2b151
EoinMcD <48068135+EoinMcD@users.noreply.github.com>
2024-07-02 19:06:38 +0100

    Big File Reorganisation

--- Assets/Scripts/ItemDatabase.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemDatabase", menuName = "Item Database")]
-public class ItemDatabase : ScriptableObject {
-    public List<Item> items;
-}
